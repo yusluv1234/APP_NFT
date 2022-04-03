0ef99713010752b9e180dacc25d4733f8d9b72b4
yusluv1234 <yusluv124@gmail.com>
2022-04-03 19:37:02 +0800

    Adding image to NFTCard compoonent

--- conponent/NFTCard.js
+++ conponent/NFTCard.js
@@ -1,9 +1,31 @@
-import { View, Text } from 'react-native'
+import { useNavigation } from '@react-navigation/core'
 import React from 'react'
+import { Image, Text, View } from 'react-native'
+import { COLORS, SHADOWS, SIZES } from '../constants'
 
-const NFTCard = () => {
+const NFTCard = ({ data }) => {
+const navigation = useNavigation();
+
   return (
-    <View>
+    <View style={{
+      backgroundColor: COLORS.white,
+      borderRadius: SIZES.font,
+      marginBottom: SIZES.extraLarge,
+      margin: SIZES.base,
+      ...SHADOWS.dark
+    }}>
+      <View style={{ width: "100%", height: 250 }}>
+        <Image
+          source={data.image}
+          resizeMode="cover"
+          style={{
+            width: "100%",
+            height: "100%",
+            borderTopLeftRadius: SIZES.font,
+            borderTopRightRadius: SIZES.font,
+          }}
+        />
+      </View>
       <Text>NFTCard</Text>
     </View>
   )
--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
-import { FlatList, SafeAreaView, Text, View } from 'react-native';
-import { FocusedStatusBar } from '../conponent';
+import { FlatList, SafeAreaView, View } from 'react-native';
+import { FocusedStatusBar, HomeHeader, NFTCard } from '../conponent';
 import { COLORS, NFTData } from "../constants";
 
 const Home = () => {
@@ -11,8 +11,22 @@ const Home = () => {
         <View style={{ zIndex: 0 }}>
           <FlatList 
             data={NFTData} 
-            renderItem={({ item }) => <Text>{item.name}</Text>} 
+            renderItem={({ item }) => <NFTCard data={item} />}
+            keyExtractor={(item) => item.id}
+            showsHorizontalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
           />
+        </View>
+        <View style={{
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          zIndex: -1,
+        }}>
+            <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+            <View style={{ flex: 1, backgroundColor: COLORS.white }} />
         </View>
       </View>
     </SafeAreaView>
