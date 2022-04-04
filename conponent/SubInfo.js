5fcd32e3d2b02fd091857df527b857cde10f2032
yusluv1234 <yusluv124@gmail.com>
2022-04-04 15:07:45 -0700

    Still of subInfo component

--- conponent/SubInfo.js
+++ conponent/SubInfo.js
@@ -1,66 +1,66 @@
-import React from 'react'
-import { Image, Text, View } from 'react-native'
-import { assets, SIZES } from '../constants'
+// import React from 'react'
+// import { Image, Text, View } from 'react-native'
+// import { assets, SIZES } from '../constants'
 
-export const NTFTitle = () => {
-  return (
-    <View>
-      <Text>NFTTitle</Text>
-    </View>
-  )
-}
+// export const NTFTitle = () => {
+//   return (
+//     <View>
+//       <Text>NFTTitle</Text>
+//     </View>
+//   )
+// }
 
-export const EthPrice = () => {
-  return (
-    <View>
-      <Text>EthPrice</Text>
-    </View>
-  )
-}
+// export const EthPrice = () => {
+//   return (
+//     <View>
+//       <Text>EthPrice</Text>
+//     </View>
+//   )
+// }
 
-export const ImageCmp = ({ imgUrl, index }) => {
-  return (
-    <Image
-      source={imgUrl}
-      resizeMode="contain"
-      style={{
-        width: 48,
-        height: 48,
-        marginLeft: index === 0 ? 0 : -SIZES.font
-      }}
-    />
-  )
-}
+// export const ImageCmp = ({ imgUrl, index }) => {
+//   return (
+//     <Image
+//       source={imgUrl}
+//       resizeMode="contain"
+//       style={{
+//         width: 48,
+//         height: 48,
+//         marginLeft: index === 0 ? 0 : -SIZES.font
+//       }}
+//     />
+//   )
+// }
 
-export const People = () => {
-  return (
-    <View style={{ flexDirection: 'row' }}>
-      {[assets.person02, assets.person03, assets.person04].map((imgUrl, index) =>(
-        <ImageCmp imgUrl={imgUrl} index={index} key={`People${index}`} />
-      ))}
-    </View>
-  )
-}
+// export const People = () => {
+//   return (
+//     <View style={{ flexDirection: 'row' }}>
+//       {[assets.person02, assets.person03, assets.person04].map((imgUrl, index) =>(
+//         <ImageCmp imgUrl={imgUrl} index={index} key={`People${index}`} />
+//       ))}
+//     </View>
+//   )
+// }
 
-export const EndDate = () => {
-  return (
-    <View>
-      <Text>End Date</Text>
-    </View>
-  )
-}
+// export const EndDate = () => {
+//   return (
+//     <View>
+//       <Text>End Date</Text>
+//     </View>
+//   )
+// }
 
-export const SubInfo = () => {
-  return (
-    <View style={{
-      width: '100%',
-      paddingHorizontal: SIZES.font,
-      marginTop: -SIZES.extraLarge,
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-    }} >
-     <People />
-     <EndDate />
-    </View>
-  )
-}
+// export const SubInfo = () => {
+//   return (
+//     <View style={{
+//       width: '100%',
+//       paddingHorizontal: SIZES.font,
+//       marginTop: -SIZES.extraLarge,
+//       flexDirection: 'row',
+//       justifyContent: 'space-between',
+//     }} >
+//      <People />
+//      <EndDate />
+//     </View>
+//   )
+// }
