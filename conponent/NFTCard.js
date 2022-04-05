58f8077631f2755dd4e542bdd8bed0650ce715ce
yusluv1234 <yusluv124@gmail.com>
2022-04-06 05:14:00 +0800

    NFTCard component still

--- conponent/NFTCard.js
+++ conponent/NFTCard.js
@@ -1,40 +1,40 @@
-// import { useNavigation } from '@react-navigation/core'
-// import React from 'react'
-// import { Image, View } from 'react-native'
-// import { assets, COLORS, SHADOWS, SIZES } from '../constants'
-// import { CircleButton } from './BUtton'
-// import { SubInfo } from './SubInfo'
+import { useNavigation } from '@react-navigation/core'
+import React from 'react'
+import { Image, View } from 'react-native'
+import { assets, COLORS, SHADOWS, SIZES } from '../constants'
+import { CircleButton } from './BUtton'
+import { SubInfo } from './SubInfo'
 
-// const NFTCard = ({ data }) => {
-// const navigation = useNavigation();
+const NFTCard = ({ data }) => {
+const navigation = useNavigation();
 
-//   return (
-//     <View style={{
-//       backgroundColor: COLORS.white,
-//       borderRadius: SIZES.font,
-//       marginBottom: SIZES.extraLarge,
-//       margin: SIZES.base,
-//       ...SHADOWS.dark
-//     }}>
-//       <View style={{ width: "100%", height: 250 }}>
-//         <Image
-//           source={data.image}
-//           resizeMode="cover"
-//           style={{
-//             width: "100%",
-//             height: "100%",
-//             borderTopLeftRadius: SIZES.font,
-//             borderTopRightRadius: SIZES.font,
-//           }}
-//         />
-//         <CircleButton
-//           imgUrl={assets.heart} right={10} top={10}
-//         />
-//       </View>
+  return (
+    <View style={{
+      backgroundColor: COLORS.white,
+      borderRadius: SIZES.font,
+      marginBottom: SIZES.extraLarge,
+      margin: SIZES.base,
+      ...SHADOWS.dark
+    }}>
+      <View style={{ width: "100%", height: 250 }}>
+        <Image
+          source={data.image}
+          resizeMode="cover"
+          style={{
+            width: "100%",
+            height: "100%",
+            borderTopLeftRadius: SIZES.font,
+            borderTopRightRadius: SIZES.font,
+          }}
+        />
+        <CircleButton
+          imgUrl={assets.heart} right={10} top={10}
+        />
+      </View>
 
-//       <SubInfo />
-//     </View>
-//   )
-// }
+      <SubInfo />
+    </View>
+  )
+}
 
-// export default NFTCard
+export default NFTCard
